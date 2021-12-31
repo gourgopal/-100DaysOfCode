b93ea8307a7e62fd6a2016e36f7495fe69760966
Gour Gopal <gourgopal@outlook.com>
2022-01-01 00:10:45 +0530

    bug fix 😁

--- removeDuplicate.cpp
+++ removeDuplicate.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string removeDuplicate(string s){
+string removeDuplicate(string s) {
     sort(s.begin(), s.end());
     vector<char> str;
     int n = s.length();
-    for (int i = 0; i < n; i++) {
+    str.push_back(s[0]);
+    for (int i = 1; i < n; i++) {
         if (s[i - 1] == s[i]) {
             continue;
         }
