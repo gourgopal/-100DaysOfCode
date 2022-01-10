f865a936f26566d1ffd1b66eb2de8dd60617d0c6
Gour Gopal <gourgopal@outlook.com>
2022-01-10 21:07:00 +0530

    Create firstUniqChar.cpp

---
+++ LeetCode/Easy/firstUniqChar.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int chars[26] = {0};
+        for (int i = 0; i < s.length(); i++) {
+            chars[s[i] - 'a']++;
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (chars[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
