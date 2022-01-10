c2b5c765b2cd53cde2cd7f8e9a38cf3ac73f592a
Gour Gopal <gourgopal@outlook.com>
2022-01-10 21:17:10 +0530

    LeetCode 383

---
+++ LeetCode/Easy/canConstruct.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int chars[26] = {0};
+        for (char c : magazine) {
+            chars[c - 'a']++;
+        }
+        for (char c: ransomNote) {
+            chars[c - 'a']--;
+            if (chars[c - 'a'] < 0) return false;
+        }
+        return true;
+    }
+};
