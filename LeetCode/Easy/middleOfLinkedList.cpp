ef4f127f0b5b668484d2fc2ebc5dcb3974a31b0d
Gour Gopal <gourgopal@outlook.com>
2022-12-03 23:28:06 +0530

    LC 876. Middle of the Linked List
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Middle of the Linked List.
    Memory Usage: 7 MB, less than 71.95% of C++ online submissions for Middle of the Linked List.

---
+++ LeetCode/Easy/middleOfLinkedList.cpp
@@ -0,0 +1,24 @@
+//876. Middle of the Linked List
+//https://leetcode.com/problems/middle-of-the-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* middle = head;
+        while (head != nullptr && head->next != nullptr) {
+            middle = middle->next;
+            head = head->next->next;
+        }
+        return middle;
+    }
+};
